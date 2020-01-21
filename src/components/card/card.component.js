f8fd6005142f816b0c597aeb518ffd30215f2234
Ram <jmaddanala@deloitte.com>
2020-01-21 19:34:05 +0530

    Add project to github

---
+++ src/components/card/card.component.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './card.styles.css';
+
+export const Card = props => {
+    const { monster } = props;
+
+    return (
+        <div className="card-container">
+            <img
+                alt="monster"
+                src={`https://robohash.org/${monster.id}?set=set2`}
+            />
+            <h2>{monster.name}</h2>
+            <p>{monster.email}</p>
+        </div>
+    );
+};
