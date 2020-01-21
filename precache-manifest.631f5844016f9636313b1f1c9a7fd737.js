9a29264c1ef81fcca3568f209c6c156e52e7ff20
Ram <jmaddanala@deloitte.com>
2020-01-21 19:46:01 +0530

    Updates

---
+++ precache-manifest.631f5844016f9636313b1f1c9a7fd737.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "470a38ace055df63560898d815adefa8",
+    "url": "/react-basics/index.html"
+  },
+  {
+    "revision": "6eb6dc4eab2af36b6f2c",
+    "url": "/react-basics/static/css/main.bcecd0e5.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/react-basics/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-basics/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6eb6dc4eab2af36b6f2c",
+    "url": "/react-basics/static/js/main.3adbb42b.chunk.js"
+  },
+  {
+    "revision": "529a42a376c903da6454",
+    "url": "/react-basics/static/js/runtime-main.0d097239.js"
+  }
+]);
